fc9c1ee7d1b170e3b6e82191b7edfce3b5a3f77e
Sennendog <sennendog@abwesend.de>
2025-04-13 12:10:14 +0200

    biglake serviceaccount access

--- tf-gcp-lakehouse/01_infrastructure/tf-bigquery.tf
+++ tf-gcp-lakehouse/01_infrastructure/tf-bigquery.tf
@@ -13,6 +13,15 @@ resource "google_bigquery_dataset" "datalake_dataset" {
 resource "google_bigquery_connection" "biglake_iceberg_connection" {
   location      = var.region
   description = "BigLake connection to GCS bucket"
-  cloud_resource {
-  }    
+  cloud_resource {}    
+}
+
+resource "google_storage_bucket_iam_member" "biglake_storage_access" {
+  bucket = google_storage_bucket.iceberg_storage.name
+  role   = "roles/storage.objectAdmin"
+  member = "serviceAccount:${google_bigquery_connection.biglake_iceberg_connection.cloud_resource[0].service_account_id}"
+
+  depends_on = [
+    google_bigquery_connection.biglake_iceberg_connection
+  ]
 }
