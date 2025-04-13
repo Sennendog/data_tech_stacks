7997bb2959e23a3cc0184bef6e2210a572831c09
Sennendog <sennendog@abwesend.de>
2025-04-13 12:43:14 +0200

    update cloud function

--- tf-gcp-lakehouse/02_cloudfunctions/gcs_trigger_function/main.py
+++ tf-gcp-lakehouse/02_cloudfunctions/gcs_trigger_function/main.py
@@ -1,28 +1,32 @@
 import os
 import requests
+from cloudevents.http import CloudEvent
+import functions_framework
 
 FLINK_CONTROLLER_ENDPOINT = "https://flink-controller.yourdomain.com/ingest"
 
-def gcs_file_trigger(event, context):
-    file_name = event['name']
-    
-    # Only trigger on files under the /landingzone/ folder
-    if not file_name.startswith("landingzone/"):
-        print(f"Ignored file outside landing zone: {file_name}")
-        return
 
-    file_path = f"gs://{event['bucket']}/{file_name}"
-    
+@functions_framework.cloud_event
+def gcs_file_trigger(cloud_event: CloudEvent):
+    """Triggered by a change in a storage bucket."""
+
+    print(f"gcs_file_trigger CloudEvent: {cloud_event}")
+    message_attributes = cloud_event.data.get("message", {}).get("attributes", {})
+    object_id = message_attributes.get("objectId")
+
+    file_name = object_id
+        
     # Extract base filename without extension
     base_filename = os.path.basename(file_name)
     target_table, _ = os.path.splitext(base_filename)
 
     payload = {
-        "gcs_file_path": file_path,
+        "gcs_file_path": file_name,
         "target_table": target_table
     }
 
     # Notify Flink controller to start job
+    print(f"Triggering Flink ingestion with payload: {payload}")
     response = requests.post(FLINK_CONTROLLER_ENDPOINT, json=payload)
 
     if response.status_code != 200:
